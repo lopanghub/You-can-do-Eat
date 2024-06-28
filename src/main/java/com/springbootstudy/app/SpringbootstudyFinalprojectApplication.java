b8249f69c7889f5e66534c4e17f1b7825478235d
HyeonHak Lee <dustmain@gmail.com>
2024-06-28 15:26:47 +0900

    pr test

--- src/main/java/com/springbootstudy/app/SpringbootstudyFinalprojectApplication.java
+++ src/main/java/com/springbootstudy/app/SpringbootstudyFinalprojectApplication.java
@@ -11,7 +11,7 @@ import jakarta.persistence.EntityListeners;
 public class SpringbootstudyFinalprojectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootstudyFinalprojectApplication.class, args);
+		SpringApplication.run(SpringbootstudyFinalprojectlication.class, args);
 	}
 
 }
